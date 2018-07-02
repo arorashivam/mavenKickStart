af56efbedf4820c264eba30b42bd7613dcec6fe3
hari_om <nischalkumarsagar@gmail.com>
2018-07-02 22:57:40 +0530

    packaged

--- src/main/java/com/foo/bar/App.java
+++ src/main/java/com/foo/bar/App.java
@@ -1,7 +1,13 @@
 package com.foo.bar;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        if(StringUtils.contains("foo","foobar")) {
+            System.out.println("maven is magic");
+        }
+        System.out.println("maven is sorcery");
     }
 }
