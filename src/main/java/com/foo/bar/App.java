ea92eaa8fbb9a26acfae5170b8527163f98b8ff5
hari_om <nischalkumarsagar@gmail.com>
2018-07-03 19:44:21 +0530

    Revert "packaged"
    This reverts commit af56efbedf4820c264eba30b42bd7613dcec6fe3.

--- src/main/java/com/foo/bar/App.java
+++ src/main/java/com/foo/bar/App.java
@@ -1,13 +1,7 @@
 package com.foo.bar;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        if(StringUtils.contains("foo","foobar")) {
-            System.out.println("maven is magic");
-        }
-        System.out.println("maven is sorcery");
     }
 }
--- src/main/java/com/foo/bar/Pojo.java
+++
@@ -1,5 +0,0 @@
-package com.foo.bar;
-
-public class Pojo {
-    String id;
-}
