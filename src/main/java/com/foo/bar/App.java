e8db2fa558222a6d469d616da2ecf86f4d6fcbd3
hari_om <nischalkumarsagar@gmail.com>
2018-07-03 20:39:36 +0530

    StringUtils used

--- src/main/java/com/foo/bar/App.java
+++ src/main/java/com/foo/bar/App.java
@@ -1,7 +1,12 @@
 package com.foo.bar;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        if(StringUtils.contains("foobar", "foo")) {
+            System.out.println("what sorcery is it.");
+        }
     }
 }
